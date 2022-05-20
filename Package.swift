355962dd35d7e92a6b6b8de7eef645b0bd8d0f3f
BeYoung <13020244+BeYoug@users.noreply.github.com>
2022-05-20 15:34:59 +0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+import PackageDescription
+
+let package = Package(
+    name: "SwiftyRSA",
+    platforms: [.iOS(.v11), .tvOS(.v11), .watchOS(.v5)],
+    products: [
+        .library(
+            name: "SwiftyRSA",
+            targets: ["SwiftyRSA"]),
+    ],
+    targets: [
+        .target(
+            name: "SwiftyRSA",
+            dependencies: []),
+        .testTarget(
+            name: "SwiftyRSATests",
+            dependencies: ["SwiftyRSA"]),
+    ]
+)
